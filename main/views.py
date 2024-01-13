d1c277861ff9c3eadba80bcb1208f45b42de435b
olavo anselmo <olavoanselmo@gmail.com>
2024-01-13 11:58:57 -0300

    V01
    Versão 01 do projeto

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,get_object_or_404,HttpResponse
+from .models import Aluno
+
+def alunoView(request):
+    alunos_list = Aluno.objects.all
+    #return HttpResponse('exibindo os alunos, sem mostrar nada. apenas a rota')
+
+    return render(request, 'main/alunos.html',{'alunos_list':alunos_list})
+
+
